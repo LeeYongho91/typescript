ef1927768fbba1090d641c9a18dcd2b8c45c409f
LeeYongho91 <skyks4848@naver.com>
2023-07-31 23:49:59 +0900

    20230731

--- class-note/6_enum.ts
+++ class-note/6_enum.ts
@@ -6,3 +6,21 @@ enum Shoes {
 
 let myShoes = Shoes.Adidas;
 console.log(myShoes); // 아디다스
+
+
+// 예제
+enum Answer {
+  Yes = 'Y',
+  No = 'N'
+}
+
+function askQuestion(answer: Answer) {
+  if(answer === Answer.Yes) {
+    console.log('정답입니다');
+  }
+  if(answer === Answer.No) {
+    console.log('오답입니다');
+  }
+}
+
+askQuestion(Answer.Yes);
